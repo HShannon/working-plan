570bd9c4f447c847019bdcb035691af68d9c443f
wangyaru <wangyaru@yidian-inc.com>
2021-02-22 11:16:03 +0800

    update: code

--- knowledge/code/code_js/dp.js
+++ knowledge/code/code_js/dp.js
@@ -133,3 +133,31 @@ function connectCore(node1, node2){
   connectCore(node2.left, node2.right)
   connectCore(node1.right, node2.left)
 }
+
+// leetcode 114
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+  if(root === null) return;
+  flatten(root.left);
+  flatten(root.right);
+  let left = root.left;
+  let right = root.right;
+  root.left = null;
+  root.right = left;
+  let p = root;
+  while(p.right !== null){
+      p = p.right;
+  }
+  p.right = right
+};
