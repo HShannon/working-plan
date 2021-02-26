ecfb0fccc5a8a8ce1e2d3df00e59bfef72ddffcb
wangyaru <wangyaru@yidian-inc.com>
2021-02-26 13:37:38 +0800

    update: code

--- knowledge/code/code_js/dp.js
+++ knowledge/code/code_js/dp.js
@@ -629,3 +629,40 @@ var isValidBSTCore = function(root, min, max){
 var isValidBST = function(root) {
   return isValidBSTCore(root, null, null)
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    if(root == null){
+      return null;
+    }
+    if(root.val == key){
+      if(root.left === null) return root.right;
+      if(root.right === null) return root.left;
+      let minNode = getMinNode(root.right);
+      root.val = minNode.val;
+      root.right = deleteNode(root.right, minNode.val)
+    } else if(root.val < key){
+      root.right = deleteNode(root.right, key)
+    } else {
+      root.left = deleteNode(root.left, key)
+    }
+
+    function getMinNode(node){
+      while(node.left !== null){
+        node = node.left;
+      }
+      return node
+    }
+};
