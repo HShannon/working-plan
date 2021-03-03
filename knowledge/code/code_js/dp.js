3c5da0de019b387541d2129e4fa9baf9d3ad1ba4
wangyaru <wangyaru@yidian-inc.com>
2021-03-03 10:22:02 +0800

    update: code

--- knowledge/code/code_js/dp.js
+++ knowledge/code/code_js/dp.js
@@ -694,75 +694,6 @@ const deserialize = (data) => {
   return buildTree(list); // 构建的入口
 };
 
-
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * function NestedInteger() {
- *
- *     Return true if this NestedInteger holds a single integer, rather than a nested list.
- *     @return {boolean}
- *     this.isInteger = function() {
- *         ...
- *     };
- *
- *     Return the single integer that this NestedInteger holds, if it holds a single integer
- *     Return null if this NestedInteger holds a nested list
- *     @return {integer}
- *     this.getInteger = function() {
- *         ...
- *     };
- *
- *     Return the nested list that this NestedInteger holds, if it holds a nested list
- *     Return null if this NestedInteger holds a single integer
- *     @return {NestedInteger[]}
- *     this.getList = function() {
- *         ...
- *     };
- * };
- */
-/**
- * @constructor
- * @param {NestedInteger[]} nestedList
- */
-var NestedIterator = function(nestedList) {
-    
-};
-
-
-/**
- * @this NestedIterator
- * @returns {boolean}
- */
-NestedIterator.prototype.hasNext = function() {
-    
-};
-
-/**
- * @this NestedIterator
- * @returns {integer}
- */
-NestedIterator.prototype.next = function() {
-    
-};
-
-/**
- * Your NestedIterator will be called like this:
- * var i = new NestedIterator(nestedList), a = [];
- * while (i.hasNext()) a.push(i.next());
-*/
-
-// var NestedIterator = function(nestedList) {
-//   var r = [], dfs = (nestedList, i = 0, nestedInteger) => {
-//       while(nestedInteger = nestedList[i++]) nestedInteger.isInteger() ? r.push(nestedInteger.getInteger()) : dfs(nestedInteger.getList())
-//   }
-//   dfs(nestedList), this.r = r, this.i = 0
-// };
-
-// NestedIterator.prototype.hasNext = function() {
-//   return this.r[this.i] !== undefined
-// };
-
 // NestedIterator.prototype.next = function() {
 //   return this.r[this.i++]
 // };
@@ -817,16 +748,26 @@ NestedIterator.prototype.next = function() {
  * @param {NestedInteger[]} nestedList
  */
 var NestedIterator = function(nestedList) {
-    
+  this.list = [];
+  this.resetList(nestedList)    
 };
 
+NestedIterator.prototype.resetList = function(arr) {
+    for(let i =0;i<arr.length;i++){
+      if(arr[i].isInteger()){
+        this.list.push(arr[i].getInteger())
+      } else {
+        this.resetList(arr[i].getList())
+      }
+    }
+};
 
 /**
  * @this NestedIterator
  * @returns {boolean}
  */
 NestedIterator.prototype.hasNext = function() {
-    
+    return this.list.length > 0
 };
 
 /**
@@ -834,7 +775,7 @@ NestedIterator.prototype.hasNext = function() {
  * @returns {integer}
  */
 NestedIterator.prototype.next = function() {
-    
+    return this.list.shift();
 };
 
 /**
