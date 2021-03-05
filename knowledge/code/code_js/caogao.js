900a4713d585a85516483fed49032165489ed7aa
wangyaru <wangyaru@yidian-inc.com>
2021-03-05 11:29:39 +0800

    update: code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -426,3 +426,26 @@ var removeNthFromEnd = function(head, n) {
   second.next = second.next.next
   return dummy.next
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let l = 0;
+  let r = numbers.length - 1;
+  while(l < r){
+    let sum = numbers[l] + numbers[r]
+    if(sum === target){
+      return [l+1, r+1]
+    } else if(sum < target){
+      l++
+    }else {
+      r--
+    }
+  }
+  return [-1, -1]
+};
+
+// 反转数组
