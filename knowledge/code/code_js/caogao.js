84ac5b866168cb7db4579cb572d2bd28618fe58c
wangyaru <wangyaru@yidian-inc.com>
2021-03-05 14:08:59 +0800

    update: code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -449,3 +449,20 @@ var twoSum = function(numbers, target) {
 };
 
 // 反转数组
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let l = 0;
+  let r = s.length - 1;
+  while(l < r){
+    let temp = s[l];
+    s[l] = s[r];
+    s[r] = temp;
+
+    l++;
+    r--;
+  }
+  return s
+};
