f988ac271c743f5fd77e3ac867b80861886b1911
wangyaru <wangyaru@yidian-inc.com>
2021-03-09 16:34:20 +0800

    update: code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -824,5 +824,20 @@ var smallestSubsequence = function(s) {
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-
+  if(nums.length <= 0){
+    return 0
+  }
+  let slow = 0;
+  let fast = 0;
+  while(fast < nums.length){
+    if(nums[fast] != nums[slow]){
+      slow++;
+      nums[slow] = nums[fast]
+    }
+    fast++
+  }
+  return slow+1
 };
+// let nums = [0,0,1,1,1,2,2,3,3,4]
+// console.log(removeDuplicates(nums))
+
