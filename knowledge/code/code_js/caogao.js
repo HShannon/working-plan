5f7d2973d77b4995233fa7d2c40a9d8195b2fcd8
wangyaru <wangyaru@yidian-inc.com>
2021-03-09 16:59:47 +0800

    update:code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -872,5 +872,16 @@ var deleteDuplicates = function(head) {
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  
+  let slow = 0;
+  let fast = 0;
+  while(fast < nums.length){
+    if(nums[fast] != val){
+      nums[slow] = nums[fast];
+      slow++;
+    }
+    fast++
+  }
+  return slow
 };
+// let nums =[3,2,2,3];
+// console.log(removeElement(nums,3))
