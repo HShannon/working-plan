59ad9e6f5ee6c5fea232c8415a7ebc7cfa7511e1
wangyaru <wangyaru@yidian-inc.com>
2021-03-10 20:34:59 +0800

    update: code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -1107,4 +1107,41 @@ var generateParenthesisCore = function(left, right, track, res){
   generateParenthesisCore(left, right-1, track, res);
   track.pop();
 }
-console.log(generateParenthesis(3))
+// console.log(generateParenthesis(3))
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var minDepth = function(root) {
+  if(root == null) return 0;
+  let q = [];
+  q.push(root);
+  let depth = 1;
+  while(q.length > 0){
+    let len = q.length
+    for(let i = 0; i<len;i++){
+      let cur = q.shift();
+      if(cur.left == null && cur.right == null){
+        return depth;
+      }
+      if(cur.left !== null){
+        q.push(cur.left);
+      }
+      if(cur.right !== null){
+        q.push(cur.right)
+      }
+    }
+    depth++;
+  }
+  return depth
+};
+
