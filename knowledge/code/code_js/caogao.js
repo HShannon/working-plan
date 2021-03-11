c9b39c6db22b7f94fcac6ddff662f268a0626ac3
wangyaru <wangyaru@yidian-inc.com>
2021-03-11 20:24:02 +0800

    update: code

--- knowledge/code/code_js/caogao.js
+++ knowledge/code/code_js/caogao.js
@@ -1272,4 +1272,45 @@ var mypow_optimize = function(a, b) {
   }
 }
 
-console.log(mypow_optimize(3,2))
+// console.log(mypow_optimize(3,2))
+
+var findDisappearedNumbers = function(nums) {
+  const n = nums.length;
+  for (const num of nums) {
+      const x = (num - 1) % n;
+      nums[x] += n;
+  }
+  const ret = [];
+  for (const [i, num] of nums.entries()) {
+      if (num <= n) {
+          ret.push(i + 1);
+      }
+  }
+  return ret;
+};
+// console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var findErrorNums = function(nums) {
+  let dup = -1;
+  for(let i = 0;i<nums.length;i++){
+    let index = Math.abs(nums[i]) - 1;
+    if(nums[index] < 0){
+      dup = nums[i]
+    }else {
+      nums[index] *= -1;
+    }
+  }
+  let missing = -1;
+  for(let i = 0;i<nums.length;i++){
+    if(nums[i] > 0 ){
+      missing = i + 1
+    }
+  }
+  return [dup, missing];
+};
+
+// console.log(findErrorNums([1,1]))
